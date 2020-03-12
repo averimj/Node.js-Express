005cecd682126f22a1ce6839149e7ac59bb67d0d
Averi Johnson <averimj@live.com>
2020-03-11 18:11:48 -0600

    add the procfile

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { projects } = require('../data/data.json');
 
 router.get('/', (req, res) => {
-  
+
   // 1. Pass all project data to 'index' template
   res.render('index', { projects } );
 });
